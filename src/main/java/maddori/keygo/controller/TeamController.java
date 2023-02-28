fc253011e30241fa74dfc1e63801a0a119001b62
hwiwonK <gnldnjs2469@gmail.com>
2023-02-28 22:53:08 +0900

    getCertainTeamDetail api modify

--- src/main/java/maddori/keygo/controller/TeamController.java
+++ src/main/java/maddori/keygo/controller/TeamController.java
@@ -1,23 +1,21 @@
 package maddori.keygo.controller;
 
+import lombok.RequiredArgsConstructor;
 import maddori.keygo.service.TeamService;
 import maddori.keygo.dto.team.TeamResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v2/teams")
 public class TeamController {
 
-    private TeamService teamService;
+    private final TeamService teamService;
 
     @GetMapping("/{teamId}")
-    public ResponseEntity<TeamResponseDto> getCertainTeamDetail(@RequestParam String teamId) {
-
+    public ResponseEntity<TeamResponseDto> getCertainTeamDetail(@PathVariable("teamId") String teamId) {
         TeamResponseDto teamResponseDto = teamService.getCertainTeam(teamId);
 
         return new ResponseEntity<>(teamResponseDto, HttpStatus.OK);
