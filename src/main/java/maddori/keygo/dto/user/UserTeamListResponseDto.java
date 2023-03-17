065e4664267bb9f769939ec4a75027e4c7b0c9fa
hwiwonK <gnldnjs2469@gmail.com>
2023-03-17 19:27:47 +0900

    [FEAT] UserTeamListResponseDto에 json property 적용(#7)

--- src/main/java/maddori/keygo/dto/user/UserTeamListResponseDto.java
+++ src/main/java/maddori/keygo/dto/user/UserTeamListResponseDto.java
@@ -1,16 +1,19 @@
 package maddori.keygo.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class UserTeamListResponseDto {
     private Long id;
-    private String team_name;
+
+    @JsonProperty("team_name")
+    private String teamName;
 
     @Builder
     public UserTeamListResponseDto(Long id, String teamName) {
         this.id = id;
-        this.team_name = teamName;
+        this.teamName = teamName;
     }
 }
