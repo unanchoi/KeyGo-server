d32beb9a124f9cdc6c146875d211544b11734883
hwiwonK <gnldnjs2469@gmail.com>
2023-03-22 17:06:39 +0900

    [FEAT] handleUnexpectedException 구현(#18)

--- src/main/java/maddori/keygo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/maddori/keygo/common/exception/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package maddori.keygo.common.exception;
 
 import maddori.keygo.common.response.BasicResponse;
 import maddori.keygo.common.response.FailResponse;
+import maddori.keygo.common.response.ResponseCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static maddori.keygo.common.response.ResponseCode.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,4 +15,12 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<BasicResponse> handleCustomException(CustomException e) {
         return FailResponse.toResponseEntity(e.getResponseCode());
     }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<BasicResponse> handleUnexpectedException(Exception e) {
+        return FailResponse.toResponseEntity(INTERNAL_SERVER_ERROR);
+    }
+
 }
+
+
