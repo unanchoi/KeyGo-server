4778b0d409d01e30b4589234555bcdbac4760305
hwiwonK <gnldnjs2469@gmail.com>
2023-03-29 19:48:46 +0900

    [FEAT] 회원 탈퇴시 해당 회원이 보낸 피드백의 from_id null로 업데이트(#37)

--- src/main/java/maddori/keygo/repository/FeedbackRepository.java
+++ src/main/java/maddori/keygo/repository/FeedbackRepository.java
@@ -2,8 +2,15 @@ package maddori.keygo.repository;
 
 import maddori.keygo.domain.entity.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 
     public void deleteById(Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update feedback f set f.from_id = null where f.from_id = :userId", nativeQuery = true)
+    void fromUserSetNull(@Param("userId") Long userId);
 }
--- src/main/java/maddori/keygo/service/AuthService.java
+++ src/main/java/maddori/keygo/service/AuthService.java
@@ -2,7 +2,7 @@ package maddori.keygo.service;
 
 import lombok.RequiredArgsConstructor;
 import maddori.keygo.common.exception.CustomException;
-import maddori.keygo.common.response.ResponseCode;
+import maddori.keygo.repository.FeedbackRepository;
 import maddori.keygo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +13,12 @@ import static maddori.keygo.common.response.ResponseCode.*;
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
+    private final FeedbackRepository feedbackRepository;
 
     @Transactional
     public void deleteUser(Long userId) {
         userRepository.delete(userRepository.findById(userId).orElseThrow(() ->
                                                     new CustomException(USER_NOT_EXIST)));
+        feedbackRepository.fromUserSetNull(userId);
     }
 }
