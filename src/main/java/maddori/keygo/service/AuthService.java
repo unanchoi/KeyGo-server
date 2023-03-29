94141dae8285ac64d96853aae2b1e982e5d0274d
hwiwonK <gnldnjs2469@gmail.com>
2023-03-29 19:06:06 +0900

    [FEAT] 회원 탈퇴(signOut) API 구현(#37)

--- src/main/java/maddori/keygo/service/AuthService.java
+++ src/main/java/maddori/keygo/service/AuthService.java
@@ -1,11 +1,22 @@
 package maddori.keygo.service;
 
 import lombok.RequiredArgsConstructor;
+import maddori.keygo.common.exception.CustomException;
+import maddori.keygo.common.response.ResponseCode;
 import maddori.keygo.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static maddori.keygo.common.response.ResponseCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
+
+    @Transactional
+    public void deleteUser(Long userId) {
+        userRepository.delete(userRepository.findById(userId).orElseThrow(() ->
+                                                    new CustomException(USER_NOT_EXIST)));
+    }
 }
